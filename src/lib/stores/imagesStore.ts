8046aca5589afbe64022fc6dc56fa6fa309a9b60
Aristide <aristide.bruneau@gmail.com>
2023-04-19 16:36:47 +0200

    small upda

--- src/lib/stores/imagesStore.ts
+++ src/lib/stores/imagesStore.ts
@@ -1,5 +1,4 @@
 import { writable } from 'svelte/store';
-import { v4 as uuidv4 } from 'uuid';
 import { toastStore } from '@skeletonlabs/skeleton';
 import { tAdd, tReset } from '$lib/strings';
 let nextId = 1;
