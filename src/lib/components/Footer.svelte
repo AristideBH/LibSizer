023f805a0022bc1721a60dd7a04a4dca3e7975f8
Aristide <aristide.bruneau@gmail.com>
2023-04-25 15:34:12 +0200

    footer min fixes

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div
-	class="container bg-surface-50-900-token py-2 px-4 flex items-center flex-col md:justify-between md:flex-row md:items-baseline text-sm border-t border-surface-200-700-token"
+	class="container bg-surface-50-900-token py-2 px-4 flex items-center flex-col md:justify-between md:flex-row md:items-baseline text-sm border-t border-surface-200-700-token !gap-y-[2px]"
 >
 	<Nav />
 	<span>
