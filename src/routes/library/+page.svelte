cc340001f48085351bddf432b9e67e11c45d2ce7
AristideBH <aristide.bruneau@gmail.com>
2023-10-04 17:56:46 +0200

    prepare work for masonry view in library

--- src/routes/library/+page.svelte
+++ src/routes/library/+page.svelte
@@ -2,6 +2,8 @@
 	import { browser } from '$app/environment';
 	import { goto } from '$app/navigation';
 	import { liveQuery } from 'dexie';
+	import Masonry from 'svelte-bricks';
+
 	import { db } from '$lib/logic/db';
 	import { getSrc, deleteImage } from '$lib/components/images';
 
@@ -11,15 +13,20 @@
 	import * as Card from '$lib/components/ui/card';
 	import FileImport from '$lib/components/images/FileImport.svelte';
 	import Loading from '$lib/components/Loading.svelte';
-	import { flyAndScale } from '$lib/logic/utils';
-	import { fly, slide } from 'svelte/transition';
+	import { slide } from 'svelte/transition';
 
 	const cardLink = (id: number | undefined) => {
 		if (!id) return;
 		goto(`/library/${id}`);
 	};
 	$: images = liveQuery(() => (browser ? db.images.toArray() : []));
+
+	let [minColWidth, maxColWidth, gap] = [100, 350, 10];
 </script>
+
+<svelte:head>
+	<title>Your uploaded library - LibSizer</title>
+</svelte:head>
 
 <main class="col-span-full">
 	<FileImport class="max-w-2xl mx-auto mt-6" />
@@ -32,7 +39,30 @@
 				<Alert.Description>Please import some with the dropzone.</Alert.Description>
 			</Alert.Root>
 		{:else}
-			<section class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
+			<Masonry items={$images} {minColWidth} {maxColWidth} {gap} let:item class="w-full">
+				<Card.Root class="flex flex-col h-full group w-full" onClick={() => cardLink(item.id)}>
+					<Card.Content class="px-0 pb-0 grow">
+						<img src={getSrc(item)} alt="" class="rounded-t-md h-full object-cover w-full" />
+					</Card.Content>
+					<Card.Footer class="flex gap-4 items-center overflow-x-auto overflow-y-hidden pt-6 ">
+						<span class="font-semibold whitespace-nowrap">
+							{item.name}
+						</span>
+						<!-- <Button
+								variant="outline"
+								size="icon"
+								class="ml-auto sticky right-0 px-3 hidden group-hover:flex z-1 -my-2"
+								on:click={(e) => {
+									e.stopPropagation();
+									deleteImage(item.id);
+								}}
+							>
+								<Trash class="h-4 w-4" />
+							</Button> -->
+					</Card.Footer>
+				</Card.Root>
+			</Masonry>
+			<!-- <section class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
 				{#each $images as image}
 					<div transition:slide>
 						<Card.Root class="flex flex-col h-full group" onClick={() => cardLink(image.id)}>
@@ -62,7 +92,7 @@
 						</Card.Root>
 					</div>
 				{/each}
-			</section>
+			</section> -->
 		{/if}
 	{:else}
 		<Loading />
