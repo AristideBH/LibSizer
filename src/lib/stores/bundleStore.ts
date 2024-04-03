7bc194e3f88a74968364d3d216b906bad9952266
AristideBH <aristide.bruneau@gmail.com>
2024-04-03 11:04:08 +0200

    Merge Redo

--- src/lib/stores/bundleStore.ts
+++
@@ -1,4 +0,0 @@
-import { writable } from 'svelte/store';
-import { persist, createIndexedDBStorage } from '@macfja/svelte-persistent-store';
-
-export const BundleSelected = persist(writable("Standard"), createIndexedDBStorage(), 'BundleSelected');
--- src/routes/+layout.ts
+++
@@ -1,9 +0,0 @@
-import type { LayoutLoad } from './$types';
-import { version } from '$app/environment';
-
-export const load = (async ({ url }) => {
-    return {
-        appVersion: version,
-        pathName: url.pathname,
-    };
-}) satisfies LayoutLoad;
