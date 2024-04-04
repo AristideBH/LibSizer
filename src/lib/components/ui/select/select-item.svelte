f0752f2e70b5815e65cbd67d1bc223fd528aeeb0
AristideBH <aristide.bruneau@gmail.com>
2024-04-04 16:14:12 +0200

    update shadcn-svelte compoenent

--- src/lib/components/ui/select/select-item.svelte
+++ src/lib/components/ui/select/select-item.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
-	import { Select as SelectPrimitive } from 'bits-ui';
 	import { Check } from 'lucide-svelte';
-	import { cn } from '$lib/logic/utils';
+	import { Select as SelectPrimitive } from 'bits-ui';
+	import { cn } from '$lib/logic/utils.js';
 
 	type $$Props = SelectPrimitive.ItemProps;
 	type $$Events = SelectPrimitive.ItemEvents;
@@ -18,7 +18,7 @@
 	{disabled}
 	{label}
 	class={cn(
-		'relative flex w-full cursor-default select-none items-center rounded-sm py-1.5 pl-8 pr-2 text-sm outline-none focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50',
+		'relative flex w-full cursor-default select-none items-center rounded-sm py-1.5 pl-8 pr-2 text-sm outline-none data-[disabled]:pointer-events-none data-[highlighted]:bg-accent data-[highlighted]:text-accent-foreground data-[disabled]:opacity-50',
 		className
 	)}
 	{...$$restProps}
@@ -31,8 +31,10 @@
 >
 	<span class="absolute left-2 flex h-3.5 w-3.5 items-center justify-center">
 		<SelectPrimitive.ItemIndicator>
-			<Check class="h-4 w-4" />
+			<Check class="w-4 h-4" />
 		</SelectPrimitive.ItemIndicator>
 	</span>
-	<slot />
+	<slot>
+		{label || value}
+	</slot>
 </SelectPrimitive.Item>
